1bb6f8bf2df844891f8ec40eebcea598a551ea24
gaurav4332 <gaurav4332@gmail.com>
2024-01-25 15:49:05 +0530

    Presist implemented

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
-import {Provider, useDispatch} from 'react-redux';
-import {mystore} from './src/redux/MyStore';
-import Main from './src/Main';
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { Provider, useDispatch } from "react-redux";
+import { mystore } from "./src/redux/MyStore";
+import Main from "./src/Main";
+import persistStore from "redux-persist/es/persistStore";
+import { PersistGate } from "redux-persist/integration/react";
+let persistor = persistStore(mystore);
 
 const App = () => {
   return (
     <Provider store={mystore}>
-      <Main />
+      <PersistGate persistor={persistor}>
+        <Main />
+      </PersistGate>
     </Provider>
   );
 };
-      
+
 export default App;
 
 const styles = StyleSheet.create({});
--- src/redux/MyStore.js
+++ src/redux/MyStore.js
@@ -1,10 +1,19 @@
-import {configureStore} from '@reduxjs/toolkit';
-import ProductReducer from './slice/ProductSlice';
-import CartReducer from './slice/CartSlice';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import ProductReducer from "./slice/ProductSlice";
+import CartReducer from "./slice/CartSlice";
+import persistReducer from "redux-persist/es/persistReducer";
+import storage from "redux-persist/lib/storage";
 
+let persistConfig = {
+  key: "root",
+  storage,
+};
+let rootReducer = combineReducers({
+  product: ProductReducer,
+  cart: CartReducer,
+});
+
+let persistedReducer = persistReducer(persistConfig, rootReducer);
 export const mystore = configureStore({
-  reducer: {
-    product: ProductReducer,
-    cart: CartReducer,
-  },
+  reducer: persistedReducer,
 });
